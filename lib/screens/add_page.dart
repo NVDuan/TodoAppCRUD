e4b934ca69b5b873804e458b2fff6b8fae61157e
NVDuan <duannv@vnpt.vn>
2023-07-04 17:00:15 +0700

    second

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 //import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
 
@@ -17,56 +18,80 @@ class _AddTodoPageState extends State<AddTodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add todo'),
+      appBar: AppBar(
+        title: Text('Add todo'),
       ),
       body: ListView(
         padding: EdgeInsets.all(20),
         children: [
-        TextField(
-          controller: titleController,
-          decoration: InputDecoration(hintText: 'title'),
-
-        ),
-        SizedBox(height: 20,),
-        TextField(
-          controller: descriptonController,
-          decoration: InputDecoration(hintText: 'Descripton'),
-          keyboardType: TextInputType.multiline,
-          minLines: 5,
-          maxLines: 8,
-        ),
-        SizedBox(height: 20,),
-        ElevatedButton(onPressed: submitData,
-        child: Text('Submit'),)
-      ],),
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'title'),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: descriptonController,
+            decoration: InputDecoration(hintText: 'Descripton'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: submitData,
+            child: Text('Submit'),
+          )
+        ],
+      ),
     );
-    
   }
+
   Future<void> submitData() async {
     final title = titleController.text;
     final description = descriptonController.text;
     final body = {
-  "title": title,
-  "description": description,
-  "is_completed": false
-};
-final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
-final uri = Uri.parse(url);
-final respone = await http.post(uri, body: jsonEncode(body),
-headers: {'Content-Type': 'application/json'});
-if (respone.statusCode == 201) {
-  print('success');
-  showSuccessMessenger('Creating Success');
-} else {
-  print('failed');
-  print(respone.body);
-}
-
-
-  
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final respone = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (respone.statusCode == 201) {
+      titleController.text = '';
+      descriptonController.text = '';
+      print('success');
+      showSuccessMessenger('Creating Success');
+    } else {
+      showErrowMessenger('Creation Failed');
+    }
   }
-  void showSuccessMessenger(String messenger){
-  final snackBar = SnackBar(content: Text(messenger));
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
+
+  void showSuccessMessenger(String messenger) {
+    final snackBar = SnackBar(
+      content: Text(
+        messenger,
+        style: TextStyle(fontSize: 16, color: Colors.white),
+      ),
+      backgroundColor: Colors.green,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrowMessenger(String messenger) {
+    final snackBar = SnackBar(
+      content:
+          Text(messenger, style: TextStyle(fontSize: 16, color: Colors.white)),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 }
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/add_page.dart';
 
@@ -9,20 +12,62 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  List<dynamic> items = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Todo List'),
-      centerTitle: true,),
-      floatingActionButton: FloatingActionButton.extended(onPressed: navigateToAddPage,
-      label: const Text('Add Todo'),
-    ));
-  }
-  void navigateToAddPage(){
-  final route = MaterialPageRoute(builder: (context) => AddTodoPage(),
-  );
-  Navigator.push(context, route);
-  
-}
-}
+        appBar: AppBar(
+          title: const Text('Todo List'),
+          centerTitle: true,
+        ),
+        body: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index] as Map;
 
+                return ListTile(
+                  leading: CircleAvatar(child: Text('${index + 1}')),
+                  title: Text(item['title']),
+                  subtitle: Text(item['description']),
+                );
+              }),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: navigateToAddPage,
+          label: const Text('Add Todo'),
+        ));
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map<String, dynamic>;
+      final result = json['items'] as List<dynamic>;
+
+      setState(() {
+        items = result;
+      });
+    } else {}
+    //print(response.statusCode);
+    //print(response.body);
+  }
+}
